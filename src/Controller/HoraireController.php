02c9ecab0f21b72405aa320a74e4fde72296a2d8
MathildeSiegwart <mathildesiegwart@hotmail.fr>
2020-09-01 08:21:21 +0200

    ajout alice

--- src/Controller/HoraireController.php
+++ src/Controller/HoraireController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Horaire;
+use App\Entity\TypeHoraire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,38 @@ class HoraireController extends AbstractController
      */
     public function index() : Response
     {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $type = new TypeHoraire;
+        $type->setNom('type2');
+
+        $em->persist($type);
+        $em->flush();
+
+        /*$horaire = new Horaire;
+        $horaire->setNom('Nom');
+        $horaire->setCommentaire('Commentaire');
+        //$horaire->setDateHeureDebut(date());
+        //$horaire->setDateHeureFin(date());
+        $horaire->setNiveauPriorite(0);
+        //$horaire->setDateCreation(date());
+        //$horaire->setDateModification(date());
+        $horaire->setType($type);
+
+
+        
+        $em->persist($horaire);
+        $em->flush();*/
+
         return $this->render('horaire/index.html.twig');
+    }
+
+    /**
+     * @Route("/horaires/create")
+     */
+    public function create()
+    {
+        return $this
     }
 }
